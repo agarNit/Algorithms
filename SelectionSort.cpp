db0067b8394d473d5c339fde276411d3199b29b7
Nitish Agarwal <agarwalnitish121@gmail.com>
2021-03-01 15:09:38 +0530

    Add cpp file for selectionSort

---
+++ SelectionSort.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+void main() {
+    cout<<'Selection Sort';
+    // Add Selection sort code here.
+}
--- insertionSort.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-void main() {
-    cout<<'Insertion Sort';
-    // Add Insertion sort code here.
-}
