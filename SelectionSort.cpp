b36905eba6002ebb09750f18c9c500121ff50179
Nitish Agarwal <agarwalnitish121@gmail.com>
2021-03-02 02:42:51 +0530

    modify selection sort

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
-void main() {
-    cout<<'Selection Sort';
-    // Add Selection sort code
+
+void Selection_Sort(int arr[], int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i] > arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;            
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    Selection_Sort(arr,n);
 }
